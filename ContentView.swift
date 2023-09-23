09e4d623137540fc615b9f7debd67c769d10a6b5
swift-superior <145513819+swift-superior@users.noreply.github.com>
2023-09-23 09:08:09 +0800

    Starting code

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS lesson 10 challenge (github)
+//
+//  Created by Jerro Lee on 23/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
