dd28fc9af549c5c4b2be85f88f88cded7295fe64
swift-superior <145513819+swift-superior@users.noreply.github.com>
2023-09-23 09:18:37 +0800

    Original voting app
    All files of the voting app are here

---
+++ iOS lesson 8 challenge/iOS lesson 8 challenge/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS lesson 8 challenge
+//
+//  Created by Jerro Lee on 28/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS lesson 8 challenge/iOS lesson 8 challenge/Voting page.swift
@@ -0,0 +1,20 @@
+//
+//  Voting page.swift
+//  iOS lesson 8 challenge
+//
+//  Created by Jerro Lee on 28/8/23.
+//
+
+import SwiftUI
+
+struct Voting_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Voting_page_Previews: PreviewProvider {
+    static var previews: some View {
+        Voting_page()
+    }
+}
